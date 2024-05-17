bfff8fda0bd9a7b698104117508de456aa2953a8
Esteban Mathías Pacheco Carrión <167585673+Estebanpacheco18@users.noreply.github.com>
2024-05-17 14:11:56 -0500

    Desarrollo del laboratorio
    Se hizo toda la tarea referente a la base de datos

---
+++ lab10/resources/views/layout.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+</head>
+<body>
+    <nav>
+        <!-- Aquí podrías incluir la barra de navegación común a todas las páginas -->
+        <ul>
+        <li><a href="/">Inicio</a></li>
+        <li><a href="/about">Acerca de</a></li>
+        <li><a href="/contact">Contacto</a></li>
+        </ul>
+        
+    </nav>
+
+    <div class="container">
+        @yield('content') <!-- Esto insertará el contenido específico de cada página -->
+    </div>
+
+    <footer>
+        <!-- Aquí podrías incluir el pie de página común a todas las páginas -->
+        <p>Copyright 2024</p>
+    </footer>
+
+</body>
+</html>
